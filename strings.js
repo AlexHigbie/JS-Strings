31813371dbdff5a904efbaa70795a7f5551b4a76
Alexander Higbie <alexanderhigbie@Alexander-Higbies-MacBook-Pro.local>
2016-12-15 16:23:39 -0700

    Strings exercise

---
+++ strings.js
@@ -0,0 +1,20 @@
+var word = prompt("What's your favorite word?");
+alert("You entered " + word + "\n" +
+ "There are " + word.length + "characters in your word." +
+"\n" + "The third character is" + word[2] +
+"\n" + "Lowercase: " + word.toLowerCase() +
+"\n" + "Uppercase: " + word.toUpperCase() +
+"\n" + "I have always loved " + word +
+"\n" + "Subword: " + word.substring(1,4));
+
+
+
+
+
+// You entered: Mango
+// There are 5 characters in the word.
+// The third character is 'n'
+// Lowercase: mango
+// Uppercase: MANGO
+// Example: I have wanted a Mango since I was a little boy.
+// Subword: ang
